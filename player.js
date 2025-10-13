2b453b91681d4d357dfc94243471cb7fb6c8afe4
felipejhony <felipejhony91@gmail.com>
2025-10-13 13:02:50 -0300

    add battle and enemy, add more stats to player

--- player.js
+++ player.js
@@ -1,10 +1,46 @@
 export class Player {
     
-    constructor(position) {
+   constructor(position = 0) {
         this.position = position;
+        this.maxHP = 20;
+        this.hp = this.maxHP;
+        this.attack = 3;
+        this.xp = 0;
+        this.level = 1;
     }
+
     move() {
         this.position++;
-        return this.position;
+    }
+
+    takeDamage(amount) {
+        this.hp -= amount;
+        if (this.hp < 0) this.hp = 0;
+    }
+
+    heal(amount) {
+        this.hp = Math.min(this.hp + amount, this.maxHP);
+    }
+
+    gainXP(amount) {
+        this.xp += amount;
+        if (this.xp >= this.level * 10) {
+            this.level++;
+            this.attack++;
+            this.maxHP += 5;
+            this.hp = this.maxHP;
+        }
+    }
+
+    isDead() {
+        return this.hp <= 0;
+    }
+
+    reset() {
+        this.position = 0;
+        this.hp = this.maxHP;
+        this.xp = 0;
+        this.level = 1;
+        this.attack = 3;
     }
 }
